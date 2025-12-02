eebc290b81ebc0d9131c21a2285d66553bbe6f09
Junlin Wang <wangjl@seas.upenn.edu>
2025-12-02 18:17:10 -0500

    Create robosuite_image_runner.py

---
+++ diffusion_policy/env_runner/robosuite_image_runner.py
@@ -0,0 +1,6 @@
+from diffusion_policy.env_runner.base_image_runner import BaseImageRunner
+
+class RobosuiteImageRunner(BaseImageRunner):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(output_dir=None)
